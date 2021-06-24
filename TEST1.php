ddb942885f36c88c5293811bee9d67327f6d2d4f
ngomcouly <84283560+ngomcouly@users.noreply.github.com>
2021-06-24 21:07:30 +0200

    commit : Couly "Mon fichier de la veille 7"

---
+++ TEST1.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>INSCRIPTION</title>
+    <link rel="stylesheet" type="text/css" href="css_page1.css">
+</head>
+<body>
+    <div class="blog_principale">
+        <div class="sous_blog">
+            <div class="ligne">
+                <h3>Inscription</h3>
+                <h5>Votre chauffeur en un clic !</h5>
+            </div>
+            <div class="ligne">
+                <button>
+                    <a href="">Continuer avec Facebook</a>
+                </button>
+            </div>
+            </div> class="ligne">
+            <label>Prénom</label>
+            <input type="text" name="Prenom">
+            >/div>
+
+            <div class="ligne ou">
+                <p>__<span style="position: relative; top:6px;">ou</span>__</p>
+            </div>
+            <div class="ligne">
+                <form action="" method="POST">
+                    <div class="ligne">
+                        <label>EMAIL</label>
+                        <input type="text" name="email" placeholder="ex@gmail.com">
+                    </div>
+                    <div class="ligne">
+                        <label>Mot de passe</label>
+                        <input type="password" name="password" placeholder="password    ">
+                    </div>
+                </form>
+            </div>
+            <div class="ligne bt">
+                <div>
+                    <a href="">J'ai déja un compte</a>
+                </div>
+                <div class="btn_inscription">
+                    <button>
+                        <a href="">S'inscrire</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
